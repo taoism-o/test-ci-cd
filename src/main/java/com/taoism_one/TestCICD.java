206b5e3ed06ef4a843446cc440dcba6e41c4ca8c
taoism-one <k506866565@gmail.com>
2021-05-31 15:48:26 +0800

    init repository

---
+++ src/main/java/com/taoism_one/TestCICD.java
@@ -0,0 +1,18 @@
+package com.taoism_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The class com.taoism_one.TestCICD is for
+ *
+ * @author kc
+ * @title test-ci-cd - com.taoism_one.TestCICD
+ * @date Create in 15:43 2021/5/31
+ */
+@SpringBootApplication
+public class TestCICD {
+    public static void main(String[] args) {
+        SpringApplication.run(TestCICD.class, args);
+    }
+}
